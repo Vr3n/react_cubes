db880d29bfc3987f00fc9ae61a1eb5ef482da2d2
vr3n <vickyspatel@gmail.com>
2022-05-09 10:11:43 +0530

    Added Wobble Mesh

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@ import "./App.scss";
 import { Canvas } from "@react-three/fiber";
 import SpinningMesh from "./Components/SpinningMesh";
 
+import {softShadows} from '@react-three/drei';
+
+softShadows();
+
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5, 2, 10] }}>
+      <Canvas colorManagement shadows camera={{ position: [-5, 2, 10], fov: 60 }}>
+        {/**/}
         <ambientLight intensity={0.4} />
-        <SpinningMesh />
+        <directionalLight 
+            castShadow
+            position={[0, 10, 0]}
+            intensity={1.5}
+            shadows={1024}
+            shadowCameraFar={50}
+            shadowCameraLeft={-10}
+            shadowCameraTop={10}
+            shadowCameraBottom={-10} />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        <group>
+            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+                <planeBufferGeometry attach="geometry" args={[100, 100]} />
+                <shadowMaterial attach="material" opacity={0.3} />
+            </mesh>
+        </group>
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightBlue" />
+        <SpinningMesh speed={6} factor={1} position={[-2, 1, -5]} color="yellow" />
+        <SpinningMesh speed={6} factor={1} position={[5, 1, -2]} color="pink" />
       </Canvas>
     </>
   );
--- src/Components/SpinningMesh.js
+++ src/Components/SpinningMesh.js
@@ -1,18 +1,18 @@
+import { MeshWobbleMaterial } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React, { useRef } from "react";
 
-const SpinningMesh = () => {
+const SpinningMesh = ({ position = [], speed= 1, factor = 0.6 ,args = [1, 1, 1], color='' }) => {
   const mesh = useRef(null);
 
   useFrame(() => {
     mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-    mesh.current.rotation.y = mesh.current.rotation.x += 0.01;
   });
 
   return (
-    <mesh ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="blue" />
+    <mesh castShadow position={position} ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <MeshWobbleMaterial speed={speed} factor={factor} attach="material" color={color} />
     </mesh>
   );
 };
